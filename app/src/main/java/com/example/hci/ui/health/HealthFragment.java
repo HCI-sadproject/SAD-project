c8dfeacc580f35e05adc22dee675124523d93424
KJY <kimjeong771@gmail.com>
2024-12-01 22:41:57 +0900

    health page UI 업데이트 및 점수 받아와 보여주기

--- app/src/main/java/com/example/hci/ui/health/HealthFragment.java
+++ app/src/main/java/com/example/hci/ui/health/HealthFragment.java
@@ -1,33 +1,30 @@
 package com.example.hci.ui.health;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hci.databinding.FragmentHealthBinding;
-import com.example.hci.ui.survey.RegularSurveyFragment;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
-
-import com.example.hci.databinding.FragmentHealthBinding;
 
 public class HealthFragment extends Fragment {
 
